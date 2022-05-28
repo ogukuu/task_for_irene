1215353852ca4cf8e0f5bbcc5682d7a7166c9159
ogukuu <ogukuu@gmail.com>
2022-05-28 20:08:55 +0300

    add save settings

--- lib/src/settings/settings_service.dart
+++ lib/src/settings/settings_service.dart
@@ -1,6 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsService {
-  Future<ThemeMode> themeMode() async => ThemeMode.system;
-  Future<void> updateThemeMode(ThemeMode theme) async {}
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  Future<ThemeMode> themeMode() async {
+    var prefs = await _prefs;
+    return MyThemeMode.getThemeMode(
+        prefs.getString(MyThemeMode.key) ?? MyThemeMode.system);
+  }
+
+  Future<void> updateThemeMode(ThemeMode theme) async {
+    var prefs = await _prefs;
+    prefs.setString(MyThemeMode.key, MyThemeMode.getNameThemeMode(theme));
+  }
+}
+
+class MyThemeMode {
+  static const String key = "settingsThemeMode";
+  static const String system = "system";
+  static const String dark = "dark";
+  static const String light = "light";
+
+  static ThemeMode getThemeMode(String nameThemeMode) {
+    switch (nameThemeMode) {
+      case dark:
+        return ThemeMode.dark;
+      case light:
+        return ThemeMode.light;
+      default:
+        return ThemeMode.system;
+    }
+  }
+
+  static String getNameThemeMode(ThemeMode themeMode) {
+    switch (themeMode) {
+      case ThemeMode.dark:
+        return dark;
+      case ThemeMode.light:
+        return light;
+      default:
+        return system;
+    }
+  }
 }
