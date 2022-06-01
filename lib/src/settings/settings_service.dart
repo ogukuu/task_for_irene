8913719e695fdee100ec8ad340ff26c4c0aceb9d
ogukuu <ogukuu@gmail.com>
2022-06-01 15:40:11 +0300

    remove shared preference

--- lib/src/settings/settings_service.dart
+++ lib/src/settings/settings_service.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
-//import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsService {
   static String settingsBoxName = "settingsBoxName";
@@ -8,30 +7,19 @@ class SettingsService {
   final Future<Box<String>> _settingsBox =
       Hive.openBox<String>(settingsBoxName);
 
-  //final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
-
   Future<ThemeMode> themeMode() async {
     var settingsBox = await _settingsBox;
-    if (settingsBox.isEmpty || !settingsBox.containsKey(themeModeSettingKey)) {
-      return MyThemeMode.defaultThemeMode;
-    } else {
-      return MyThemeMode.getThemeMode(settingsBox.get(themeModeSettingKey)!);
-    }
-  }
-  /*var prefs = await _prefs;
     return MyThemeMode.getThemeMode(
-        prefs.getString(MyThemeMode.key) ?? MyThemeMode.system);*/
+        settingsBox.get(themeModeSettingKey) ?? MyThemeMode.system);
+  }
 
   Future<void> updateThemeMode(ThemeMode theme) async {
     var settingsBox = await _settingsBox;
     settingsBox.put(themeModeSettingKey, MyThemeMode.getNameThemeMode(theme));
-    /*var prefs = await _prefs;
-    prefs.setString(MyThemeMode.key, MyThemeMode.getNameThemeMode(theme));*/
   }
 }
 
 class MyThemeMode {
-  static const String key = "settingsThemeMode";
   static const String system = "system";
   static const String dark = "dark";
   static const String light = "light";
