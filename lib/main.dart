1fef1eb2f136efd75554dbfecbb6a77216373477
ogukuu <ogukuu@gmail.com>
2022-06-03 10:06:55 +0300

    change GlobalVar

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:task_for_irene/src/repository/hive/hive_task_repository.dart';
+import 'package:task_for_irene/src/global_var.dart';
 
 import 'src/app.dart';
-import 'src/app_controller.dart';
-import 'src/settings/settings_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final repository = HiveTaskRepository();
-  await repository.init();
-  final appController =
-      AppController(settingsService: SettingsService(), repository: repository);
-  await appController.loadSettings();
-  appController.loadTasks();
-  runApp(MyApp(appController: appController));
+  await GlobalVar.appController.repository.init();
+  await GlobalVar.appController.settingsService.init();
+  await GlobalVar.appController.loadSettings();
+  GlobalVar.appController.loadTasks();
+  runApp(MyApp(appController: GlobalVar.appController));
 }
